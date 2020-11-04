40386f38abb96562e031800f309a493dfdad31cc
zhaodesong <zhaodesong1996@gmail.com>
2020-11-04 18:38:56 +0800

    详细申报接口：临时提交，未完成

--- src/main/java/com/example/nybproject/meet/controller/HandinController.java
+++ src/main/java/com/example/nybproject/meet/controller/HandinController.java
@@ -2,15 +2,17 @@ package com.example.nybproject.meet.controller;
 
 import com.example.nybproject.meet.mapper.EasyMapper;
 import com.example.nybproject.meet.pojo.EasyMeet;
+import com.example.nybproject.meet.pojo.ResDetailMeet;
 import com.example.nybproject.meet.result.HttpResult;
+import com.example.nybproject.meet.result.HttpResultCodeEnum;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
 import java.time.LocalDateTime;
 
 /**
- *
  * 用户上报功能控制
  * 10.29 完成第一版，id暂时用123123代替
  */
@@ -38,6 +40,20 @@ public class HandinController {
 
         return HttpResult.of(400);
 
+    }
+
+    @CrossOrigin
+    @PostMapping("/detail")
+    @ResponseBody
+    public HttpResult<Void> easy(ResDetailMeet resDetailMeet) {
+        MultipartFile preExpoFile = resDetailMeet.getPreExpoFile();
+        MultipartFile investmentPlanFile = resDetailMeet.getInvestmentPlanFile();
+        if (preExpoFile.isEmpty() || investmentPlanFile.isEmpty()) {
+            // TODO 返回码待补充
+            return HttpResult.of(HttpResultCodeEnum.SYSTEM_ERROR);
+        }
+        // 先保存两个文件到mongo,获得对应的ID后，将其他数据保存到mysql
+        return HttpResult.of();
     }
 
 }
